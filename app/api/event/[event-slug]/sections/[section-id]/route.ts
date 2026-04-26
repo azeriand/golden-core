8e06ee12f46998a3b4f55802ffda892fe6662d17
azeriand <andrearomeracosta4@gmail.com>
2026-04-26 21:25:43 +0200

    feat(section): add put function

--- app/api/event/[event-slug]/sections/[section-id]/route.ts
+++ app/api/event/[event-slug]/sections/[section-id]/route.ts
@@ -1,1 +1,43 @@
 //Modify/delete data for a specific event section
+
+import pool from '@/lib/db';
+import { NextRequest } from 'next/server';
+
+export async function PUT(request: NextRequest, { params }: { params: Promise<{ "event-slug": string } & { "section-id": string }> }) {
+  const { "event-slug": eventSlug, "section-id": sectionId } = await params;
+  const { name, startDate, finishDate, sectionOrder } = await request.json();
+
+  if (!name) {
+    return new Response('Name missing', {
+        status: 400,
+        headers: { 'Content-Type': 'text/plain' }
+    })
+  }
+  
+  if (!startDate) {
+    return new Response('Start date missing', {
+        status: 400,
+        headers: { 'Content-Type': 'text/plain' }
+    })
+  }
+ 
+  try {
+    const result = await pool.query(
+      `UPDATE sections s
+       SET section_name = $1, start_date = $2, finish_date = $3, section_order = $4
+       FROM events e
+       WHERE e.event_id = s.event_id and e.event_slug = $5 and s.section_id = $6`,
+      [name, startDate, finishDate, sectionOrder, eventSlug, sectionId]
+    );
+    return new Response('OK', {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' }
+    });
+} catch (error: unknown) {
+    return new Response(error.message, {
+      status: 400,
+      headers: { 'Content-Type': 'application/json' }
+    });
+  }
+  
+}
