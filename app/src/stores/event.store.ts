a6fae7731aa49938c3e18a98e86e0fd2ef452681
azeriand <andrearomeracosta4@gmail.com>
2026-08-03 13:12:23 +0200

    refactor(event): refactor interfaces

--- app/src/stores/event.store.ts
+++ app/src/stores/event.store.ts
@@ -1,53 +1,41 @@
 import { create } from "zustand";
 import axios from "axios";
-import { Section } from "@/app/dto/section";
+import { Event } from "@/app/dto/event";
 
-interface EventState {
-  event_id: number;
-  event_name: string;
-  event_slug: string;
-  event_date: string;
-  sections: any[];
+
+interface EventStore {
+  event: Event | null;
+  loading: boolean;
   fetchEvent: (event_slug: string) => Promise<void>;
-  addEvent: () => void;
-  removeEvent: () => void;
-  updateEvent: () => void;
 }
+const useEventStore = create<EventStore>((set) => ({
 
-const useEventStore = create<EventState>((set) => ({
+    event: null,
+    loading: true,
 
-    event_id: 0,
-    event_name: "",
-    event_slug: "",
-    event_date: "",
-    sections: [],
+    fetchEvent: async (event_slug: string) => {
 
-  fetchEvent: async (event_slug: string) => {
-    try {
-      const response = await axios.get(`/api/event/${event_slug}`);
+        set({
+            loading: true,
+        });
+        
+        try {
+            const response = await axios.get(`/api/event/${event_slug}`);
 
-      console.log(response.data);
-      set({
-        event_id: response.data.event_id,
-        event_name: response.data.event_name,
-        event_date: response.data.event_date,
-        event_slug: response.data.event_slug,
-        sections: response.data.sections.map((section: Section) => section),
-      });
-    } catch (error) {
-      set({
-        event_id: 0,
-        event_name: "Boda No Encontrada",
-        event_slug: "",
-        event_date: "",
-        sections: [],
-      });
-    console.error("Error", error);
-  }
-  },
-  addEvent: () => {},
-  removeEvent: () => {},
-  updateEvent: () => {},
+            set({
+                event: response.data,
+                loading: false,
+            });
+
+        } catch (error) {
+            set({
+                event: null,
+                loading: false,
+            });
+
+            console.error("Error", error);
+        }
+    },
 
 }));
 
