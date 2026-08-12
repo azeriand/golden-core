28af708ba6bd585a594ab38f31641373e8324863
azeriand <andrearomeracosta4@gmail.com>
2026-08-12 11:58:17 +0200

    feat(media): update POST endpoint

--- app/api/event/[event-slug]/media/route.ts
+++ app/api/event/[event-slug]/media/route.ts
@@ -3,19 +3,21 @@ import pool from '@/lib/db';
 import { NextRequest } from 'next/server';
 import jwt from 'jsonwebtoken'
 import { cookies } from 'next/headers'
+import { put } from "@vercel/blob";
 
 export async function POST(request: NextRequest, { params }: { params: Promise<{ "event-slug": string }> }) {
     const { "event-slug": eventSlug } = await params;
-    const { content, date, sectionId, eventId} = await request.json();
-
     const cookieStore = await cookies()
     const token = cookieStore.get('auth_token')?.value
 
-    if (!content) {
-        return new Response('Media content missing', {
+    const formData = await request.formData();
+    const file = formData.get("file");
+    const date = formData.get("date") as string;
+
+    if (!(file instanceof File)) {
+        return new Response("File missing", {
             status: 400,
-            headers: { 'Content-Type': 'text/plain' }
-        })
+        });
     }
   
     if (!date) {
@@ -47,11 +49,35 @@ export async function POST(request: NextRequest, { params }: { params: Promise<{
     }
     console.log('DECODED', decoded)
     const userId = decoded.userId
+
+    const eventResult = await pool.query(
+        `
+        SELECT event_id
+        FROM events
+        WHERE event_slug = $1
+        `,
+        [eventSlug]
+    );
+
+    if (eventResult.rows.length === 0) {
+        return new Response("Event not found", {
+            status: 404,
+        });
+    }
+
+    const eventId = eventResult.rows[0].event_id;
+
+    const blob = await put(file.name, file, {
+        access: "public",
+    });
+
+    const content = blob.url;
+
     const result = await pool.query(
         `INSERT INTO media (content, date, user_id, section_id, event_id)
-        VALUES ($1, $2, $3, $4, $5)
+        VALUES ($1, $2, $3, NULL, $4)
         RETURNING *`,
-        [content, date, userId, sectionId, eventId]
+        [content, date, userId, eventId]
     );
 
     return new Response(JSON.stringify(result.rows[0]), {
