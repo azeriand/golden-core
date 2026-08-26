9cec434a42c449bfdfa852bcb42dbc8c321afb8b
azeriand <andrearomeracosta4@gmail.com>
2026-08-26 11:15:36 +0200

    chore(media): media render optimization

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,25 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["sharp"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.public.blob.vercel-storage.com",
+      },
+      {
+        protocol: "https",
+        hostname: "img.magnific.com",
+      },
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+      },
+    ],
+    formats: ["image/avif", "image/webp"],
+    deviceSizes: [640, 750, 828, 1080, 1200],
+    imageSizes: [256, 384, 512],
+  },
 };
 
 export default nextConfig;
